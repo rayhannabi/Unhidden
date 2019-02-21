5b5b765ea55d385c948259fac4eea7350cf216e8
Rayhan Nabi <rayhanjanam@gmail.com>
2019-02-22 03:15:09 +0600

    Add ShellResult

---
+++ RNShell/ShellResult.swift
@@ -0,0 +1,15 @@
+//
+//  ShellResult.swift
+//  RNShell
+//
+//  Created by Rayhan Nabi on 2/22/19.
+//  Copyright © 2019 Rayhan. All rights reserved.
+//
+
+import Foundation
+
+public struct ShellResult {
+  var output: String?
+  var error: String?
+  var status: Int
+}
