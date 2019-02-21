a30f05eb3f477c7191de05b6900a1b6aa84e3b88
Rayhan Nabi <rayhanjanam@gmail.com>
2019-02-22 03:12:23 +0600

    Add RNShell class

---
+++ RNShell/RNShell.swift
@@ -0,0 +1,13 @@
+//
+//  RNShell.swift
+//  RNShell
+//
+//  Created by Rayhan Nabi on 2/22/19.
+//  Copyright © 2019 Rayhan. All rights reserved.
+//
+
+import Cocoa
+
+class RNShell {
+  
+}
--- RNShellTests/RNShellTests.swift
+++ RNShellTests/RNShellTests.swift
@@ -10,25 +10,20 @@ import XCTest
 @testable import RNShell
 
 class RNShellTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  
+  private var shell: RNShell!
+  
+  override func setUp() {
+    super.setUp()
+    shell = RNShell()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func test_shellInitialized() {
+    XCTAssertNotNil(shell, "Shell instance should not be nil")
+  }
+  
 }
