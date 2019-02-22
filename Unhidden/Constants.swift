e21b36620ee63bdf50d49ebb27fed4031de6e183
Rayhan Nabi <rayhanjanam@gmail.com>
2019-02-22 21:00:00 +0600

    Add Constants

---
+++ Unhidden/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Unhidden
+//
+//  Created by Rayhan Nabi on 2/22/19.
+//  Copyright © 2019 Rayhan. All rights reserved.
+//
+
+import Cocoa
+
+struct Constants {
+  
+  struct Colors {
+    static var switchOn = NSColor(calibratedRed: 69/255, green: 220/255, blue: 92/255, alpha: 1.0)
+    static var switchOff = NSColor(calibratedRed: 255/255, green: 102/255, blue: 102/255, alpha: 1.0)
+  }
+  
+}
